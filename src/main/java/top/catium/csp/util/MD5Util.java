9ad674567c949a48c5c4244df628dcb5c98521cc
Tony Shang <64829162+TonyNomoney@users.noreply.github.com>
2023-01-31 13:12:32 +0800

    Create MD5Util.java

---
+++ src/main/java/top/catium/csp/util/MD5Util.java
@@ -0,0 +1,22 @@
+package top.catium.csp.util;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Util {
+    public static String fromString(String str) {
+        byte[] digest = null;
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("md5");
+            digest  = md5.digest(str.getBytes("utf-8"));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String md5Str = new BigInteger(1, digest).toString(16);
+        return md5Str;
+    }
+}
